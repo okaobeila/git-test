d3a763d4e6fd4361c9538d944c61f2fa46ac00c5
Andy <1654019385@qq.com>
2022-09-03 17:06:41 +0800

    push commit4

--- src/main/java/com/gec/git/GitTest.java
+++ src/main/java/com/gec/git/GitTest.java
@@ -9,7 +9,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("push test2");
-
         System.out.println("push test3");
     }
 }
